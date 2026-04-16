f9a7dd94df47189b2fa0896aea3f86943bea252c
DESKTOP-37PPCAA\ULTRA <ev1042290@gmail.com>
2026-04-16 17:24:13 +0900

    (윈도우 불안정 이슈로 백업) 1. UI/메시분쇄 에셋 테스트

---
+++ Assets/Boxal/Scripts/TestClass.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace SampleTest
+{
+    public class TestClass : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello World");
+        }
+
+        private void Update()
+        {
+            Debug.Log("Hello Update");
+        }
+    }
+
+}
